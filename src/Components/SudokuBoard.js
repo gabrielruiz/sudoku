c8fcad87cf230bb32179c0510610fcf13416b3dc
Gabriel Ruiz <gabrielruiz@genesisappsweb.com>
2021-11-07 22:04:43 -0300

    Applying changes to play or pause the sudoku game restarting the time counter.

--- src/Components/SudokuBoard.js
+++ src/Components/SudokuBoard.js
@@ -7,6 +7,16 @@ import {opponentSolvedTime} from '../Library/Sudoku'
 
 class SudokuBoard extends Component {
 
+    state = {
+        isTimePaused: false
+    }
+
+    timerChanged(value) {
+        this.setState({
+            isTimePaused: value
+        })
+    }
+
     render(){
         const {puzzle, onChange, handleSolveClick, hiddingSolveBtn} = this.props,
               isSolved = puzzle.solvedTime;
@@ -20,8 +30,8 @@ class SudokuBoard extends Component {
                     )}/>
                 }
                 {isSolved && <SudokuResult sudoku={puzzle}/>}
-                <SudokuTimer start={puzzle.startTime} isAChallenge={(puzzle.challengerStartTime)} isSolved={isSolved}/>
-                <div className="board-grid">
+                <SudokuTimer start={puzzle.startTime} isAChallenge={(puzzle.challengerStartTime)} isSolved={isSolved} onChange={this.timerChanged.bind(this)}/>
+                {!this.state.isTimePaused && <div className="board-grid">
                     {puzzle.rows.map((row, i)=>(
                         <div key={row.index} className="row">
                             {row.cols.map((field)=>(
@@ -29,7 +39,8 @@ class SudokuBoard extends Component {
                                 ))}
                         </div>
                     ))}
-                </div>
+                </div>}
+                {this.state.isTimePaused && <h2 className="board-grid-paused">Game Paused!</h2>}
                 <br/>
                 {!hiddingSolveBtn && <button onClick={handleSolveClick}>Solve it Magically!</button>}
                 
--- src/Components/SudokuTimer.js
+++ src/Components/SudokuTimer.js
@@ -4,8 +4,12 @@ import {timeInHHMMSS, removeSavedPuzzle, shareURL} from '../Library/Sudoku';
 class SudokuTimer extends Component {
 
     state = {
-        elapsed: 0
+        elapsed: 0,
+        isTimePaused: false,
+        pausedTime: 0,
     }
+
+    interval;
 
     componentDidMount() {
         this.interval = setInterval(()=>{
@@ -19,6 +23,40 @@ class SudokuTimer extends Component {
 
     componentWillUnmount() {
         delete this.interval;
+    }
+
+    saveCurrentTime() {
+        clearInterval(this.interval);
+        const pausedTime = this.state.elapsed;
+        this.setState({
+            pausedTime
+        })
+    }
+
+    reStartTime() {
+        const pausedTime = this.state.pausedTime;
+        const newTime = new Date().getTime();
+        this.interval = setInterval(()=>{
+            this.setState({
+                elapsed: Math.floor(
+                    ( (new Date().getTime() - newTime) / 1000 ) + pausedTime
+                )
+            });
+        }, 1000);
+    }
+
+    playPause() {
+        const stateIsTimePaused = this.state.isTimePaused;
+        const isTimePaused = !stateIsTimePaused;
+        this.setState({
+            isTimePaused
+        });
+        this.props.onChange(isTimePaused);
+        if (isTimePaused) {
+            this.saveCurrentTime();
+        } else {
+            this.reStartTime();
+        }
     }
 
     newSudoku() {
@@ -38,11 +76,12 @@ class SudokuTimer extends Component {
 
     render() {
         const {elapsed} = this.state,
-              {isAChallenge, isSolved} = this.props;
+              {isAChallenge, isSolved} = this.props,
+              timerStatusClassName = 'pause-sudoku-btn sudoku-time-action ';
 
         return (
             <section className="sudoku-timer">
-                {!isSolved && <h3>{isAChallenge ? 'Your Time': 'Time'}: {timeInHHMMSS(elapsed)}</h3>}
+                {!isSolved && <h3>{isAChallenge ? 'Your Time': 'Time'}: {timeInHHMMSS(elapsed)}&nbsp;&nbsp;&nbsp;<button className={this.state.isTimePaused ? timerStatusClassName + "sudoku-play": timerStatusClassName + "sudoku-pause" } onClick={this.playPause.bind(this)}>{this.state.isTimePaused ? 'Play': 'Pause'}</button></h3>}
                 <p className="sudoku-actions">
                     <button className="new-sudoku-btn icon icon-sudoku" onClick={this.newSudoku}>New Sudoku Board</button>
                     {!isSolved && <button className="share-link-btn icon icon-link" onClick={this.shareSudokuBoard.bind(this)}>Share Sudoku Board</button>}
